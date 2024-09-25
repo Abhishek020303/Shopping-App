86e483c4329bcb7b51d8fec2cf6c3a795b3217c9
Abhishek020303 <abhishek.expertise@gmail.com>
2024-09-25 13:15:18 +0530

    Final Commit

--- app/src/main/java/com/abhishek/onlineshop/Helper/TinyDB.java
+++ app/src/main/java/com/abhishek/onlineshop/Helper/TinyDB.java
@@ -84,7 +84,6 @@ public class TinyDB {
             bitmapFromPath = BitmapFactory.decodeFile(path);
 
         } catch (Exception e) {
-            // TODO: handle exception
             e.printStackTrace();
         }
 
